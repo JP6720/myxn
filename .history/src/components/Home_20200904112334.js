e9c02ae70e74a5e94a0afa3c2da954d02445fa94
Mac <mac@Mac-mini-2.local>
2020-09-04 12:02:34 +0530

    New Commit

---
+++ .history/src/components/Home_20200904112334.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text ,AsyncStorage} from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+import auth from '@react-native-firebase/auth';
+import {LoginManager,AccessToken,GraphRequestManager,GraphRequest} from 'react-native-fbsdk'
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
+export default function Home({ navigation }) {
+
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+     
+    useEffect(() => {
+        
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+     
+    if (initializing) return null;
+     
+    if (!user) {
+        return navigation.navigate('Login');
+    }
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Welcome {user.email}</Text>
+        </View>
+    );
+}
+
+function customFacebookLogout(){
+    console.log('ss')
+    AsyncStorage.removeItem('token').then(()=>{
+        navigation.navigate('Login')
+      })
+    var current_access_token = '';
+    AccessToken.getCurrentAccessToken().then((data) => {
+      current_access_token = data.accessToken.toString();
+    }).then(() => {
+      let logout =
+      new GraphRequest(
+        "me/permissions/",
+        {
+            accessToken: current_access_token,
+            httpMethod: 'DELETE'
+        },
+        (error, result) => {
+            if (error) {
+                console.log('Error fetching data: ' + error.toString());
+            } else {
+                LoginManager.logOut();
+            }
+        });
+      new GraphRequestManager().addRequest(logout).start();
+      
+      
+    })
+    .catch(error => {
+      console.log(error)
+    });      
+  }
+
+Home.navigationOptions = ({ navigation }) => ({
+    title: 'Home',
+    headerRight: () => <Button  
+            buttonStyle={{ padding: 0, marginRight: 20, backgroundColor: 'transparent' }}
+            icon={
+                <Icon
+               
+                name='logout'
+                type='antdesign'
+                color='white'
+              />
+            } title="Sign Out"
+            onPress={() => {auth().signOut()
+              
+                try {
+                 GoogleSignin.revokeAccess();
+                  GoogleSignin.signOut();
+                  AsyncStorage.removeItem('token').then(()=>{
+                    navigation.navigate('Login')
+                  })
+                //   this.setState({ userInfo: null });
+                
+                // Remove the user from your app's state as well
+                } catch (error) {
+                  console.error(error);
+                }
+              
+            customFacebookLogout()
+            navigation.navigate('Login')}} />,
+});
--- ios/myxn/AppDelegate.m
+++ ios/myxn/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -53,6 +54,10 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application openURL:url options:options] || [RNGoogleSignin application:application openURL:url options:options];
 }
 
 @end
